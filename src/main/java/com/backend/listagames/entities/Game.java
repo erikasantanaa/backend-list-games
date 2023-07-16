5968ca5d2955298102ca195902bb58150ae0c3cf
erikasantanaa <erikamariasantanaa@gmail.com>
2023-07-16 16:17:32 -0300

    feat: Implementando modelo de domínio

--- src/main/java/com/backend/listagames/entities/Game.java
+++ src/main/java/com/backend/listagames/entities/Game.java
@@ -9,10 +9,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+//mapeando
 @Entity
 @Table(name = "tb_game")
 public class Game {
 	
+	//mapeando
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
